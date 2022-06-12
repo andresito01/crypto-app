6b9ecbdfdc0fd20f516bef370f6fa834d1f53010
Andres Morocho <andresmorocho267@gmail.com>
2022-06-11 22:34:08 -0700

    Created a CoinList file to display data and added a search functionality.

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./CoinList.css";
 
-const CoinList = (props) => {
-  return <div></div>;
+const CoinList = ({
+  id,
+  icon,
+  coinName,
+  coinSymbol,
+  price,
+  marketCap,
+  priceChange,
+}) => {
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <img alt="coin-icon" src={icon}></img>
+          <p className="coin-name">{coinName}</p>
+          <p className="coin-symbol">{coinSymbol}</p>
+          <p className="coin-price">$ {price.toFixed(2)}</p>
+          {priceChange < 0 ? (
+            <p className="coin-price-change red">{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="coin-price-change green">{priceChange.toFixed(2)}%</p>
+          )}
+          <p className="coin-market-cap">$ {marketCap.toLocaleString()}</p>
+          <button className="more-info-button">More Info</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CoinList;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 import Axios from "axios";
 import Refresh from "../images/refresh.png";
+import CoinList from "../components/CoinList.js";
 
 const Home = () => {
   const [coins, setCoins] = useState([]);
-
+  const [searchValue, setSearchValue] = useState("");
   const url =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
@@ -17,16 +17,28 @@ const Home = () => {
     });
   };
 
+  const filterCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  const handleSearch = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   useEffect(() => {
     getCoinRequest();
   }, []);
 
   return (
-    <div className="App">
+    <div className="screen">
       <div className="header-container">
         <h1>Welcome to the CryptoChecker</h1>
         <div className="button-container">
-          <input placeholder="Search for a Coin" type="text" />
+          <input
+            placeholder="Search for a Coin"
+            type="text"
+            onChange={handleSearch}
+          />
           <img
             onClick={getCoinRequest}
             src={Refresh}
@@ -34,7 +46,19 @@ const Home = () => {
           ></img>
         </div>
       </div>
-      <div className="coinlist-container"></div>
+      <div className="coinlist-container">
+        {filterCoins.map((coins) => (
+          <CoinList
+            id={coins.id}
+            icon={coins.image}
+            coinName={coins.name}
+            coinSymbol={coins.symbol}
+            price={coins.current_price}
+            marketCap={coins.market_cap}
+            priceChange={coins.price_change_percentage_24h}
+          />
+        ))}
+      </div>
     </div>
   );
 };
